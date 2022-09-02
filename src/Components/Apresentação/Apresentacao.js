98f2fb9f89f7a13c500709f50d6d6e4b4f72132b
Gustavo Sovrani <84320223+GustavoS10@users.noreply.github.com>
2022-09-02 10:18:01 -0300

    Update Apresentacao.js

--- src/Components/Apresentação/Apresentacao.js
+++ src/Components/Apresentação/Apresentacao.js
@@ -1,51 +1,17 @@
 import { Container } from 'reactstrap'
+import React from 'react';
 import "./Apresentacao.css"
 
 const Apresentacao = () => {
   return (
     <>
       <Container fluid id='container'>
-        {/* <h1 className="name"> */}
-        {/* <span className='name'>
-          <div className="name2">
-            <div>H</div>
-            <div>i</div>
-            <div>,</div>
-          </div> */}
-        {/* </span> */}
-        {/* </h1> */}
-        {/* <br /> */}
-        {/* <h1 className="name"> */}
-        {/* <span className="name"> */}
-          {/* <div>I</div>
-          <div>'</div>
-          <div>m&nbsp;</div>
-          <div>G</div>
-          <div>u</div>
-          <div>s</div>
-          <div>t</div>
-          <div>a</div>
-          <div>,</div> */}
-        {/* </span> */}
-        {/* </h1> */}
-        {/* <br /> */}
-        {/* <h1 className="name"> */}
-        {/* <span className="name"> */}
-          {/* <div>w</div>
-          <div>e</div>
-          <div>b&nbsp;</div>
-          <div>d</div>
-          <div>e</div>
-          <div>v</div>
-        </span> */}
-        {/* </h1> */}
         <h1>H'i,<br /></h1>
         <h1>I'm Gusta, <br /></h1>
         <h1>Web Dev</h1>
         <br />
         <span id="job">Front End Developer / React.Js Júnior</span>
       </Container>
-
     </>
   )
 }
