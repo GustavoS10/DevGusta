b951e48907b3c59940dac8b7cb83ffc9a8905e9d
Gustavo Sovrani <84320223+GustavoS10@users.noreply.github.com>
2022-09-04 13:05:09 -0300

    Update Footer.js

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
     <div>
         <Container fluid id='FContainer'>
             <h1>Powered by Gustavo Sovrani&copy; Dev. Front End Jr 2022</h1>
-            <h1>Uploaded by Heroku&copy;</h1>
+            <h1>Uploaded by Vercel&copy;</h1>
         </Container>
     </div>
   )
